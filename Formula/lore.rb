6730b71f0c141542e474c90c58f601c443385f4d
Chris <awfulrofl@gmail.com>
2026-01-09 20:10:41 -0700

    lore 0.1.6
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/lore.rb
+++ Formula/lore.rb
@@ -1,8 +1,8 @@
 class Lore < Formula
     desc "Capture AI coding sessions and link them to git commits"
     homepage "https://github.com/varalys/lore"
-    url "https://github.com/varalys/lore/archive/refs/tags/v0.1.5.tar.gz"
-    sha256 "d18e563eada114c6d34b5c73a7d9fea0ecf088df39e10c9a413ceb18e49d4228"
+    url "https://github.com/varalys/lore/archive/refs/tags/v0.1.6.tar.gz"
+    sha256 "3b4f59b7562733eba6416b6d22858e73edf016060760b1d39b853192f6b3042f"
     license "MIT"
 
     depends_on "rust" => :build
