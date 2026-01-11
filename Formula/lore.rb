c6911fb6c44f50e32b7e7d8b66e747dafa599fdf
Chris <awfulrofl@gmail.com>
2026-01-10 17:22:13 -0700

    Update lore.rb

--- Formula/lore.rb
+++ Formula/lore.rb
@@ -3,20 +3,12 @@ class Lore < Formula
     homepage "https://github.com/varalys/lore"
     url "https://github.com/varalys/lore/archive/refs/tags/v0.1.6.tar.gz"
     sha256 "3b4f59b7562733eba6416b6d22858e73edf016060760b1d39b853192f6b3042f"
-    license "MIT"
+    license "Apache-2.0"
 
     depends_on "rust" => :build
 
     def install
       system "cargo", "install", *std_cargo_args
-    end
-
-    service do
-      run [opt_bin/"lore", "daemon", "start", "--foreground"]
-      keep_alive true
-      log_path var/"log/lore.log"
-      error_log_path var/"log/lore.log"
-      working_dir HOMEBREW_PREFIX
     end
 
     test do
